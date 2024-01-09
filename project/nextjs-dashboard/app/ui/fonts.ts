f789469461bdeaa66fb20f7ae60dda2ccfbad97a
puy4 <puy4@mcmaster.ca>
2024-01-08 19:50:34 -0500

    init

---
+++ project/nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Poppins,Lusitana } from 'next/font/google';
+
+export const poppins = Poppins({ weight: '300', subsets: ['latin'] });
+
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
---
+++ project/nextjs-dashboard/app/ui/logo.tsx
@@ -0,0 +1,21 @@
+import CardsClubIcon from 'mdi-react/CardsClubIcon';
+import CardsDiamondOutlineIcon from 'mdi-react/CardsDiamondOutlineIcon';
+import CardsHeartOutlineIcon from 'mdi-react/CardsHeartOutlineIcon';
+import CardsSpadeIcon from 'mdi-react/CardsSpadeIcon';
+import CardsPlayingOutlineIcon from 'mdi-react/CardsPlayingOutlineIcon';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function Logo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <CardsPlayingOutlineIcon className="h-12 w-12 rotate-[0deg]" />
+      <p className="text-[44px]">4Z06 Group 16: Duodecimal Cribbage  </p>
+      <CardsClubIcon className="h-12 w-12 rotate-[0deg]" />
+      <CardsDiamondOutlineIcon className="h-12 w-12 rotate-[0deg]" />
+      <CardsHeartOutlineIcon className="h-12 w-12 rotate-[0deg] " />
+      <CardsSpadeIcon className="h-12 w-12 rotate-[0deg] " />
+    </div>
+  );
+}
