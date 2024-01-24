46f3b875e225329a447082ac27f08fff13fe5dc9
puy4 <puy4@mcmaster.ca>
2024-01-24 00:33:44 -0500

    fetch data

--- app/client/lobby.tsx
+++ app/client/lobby.tsx
@@ -18,26 +18,24 @@ function Lobby(){
   const [members, setMembers] = useState<Array<Ably.Types.PresenceMessage>>([]);
   
 
-
   const client = new Ably.Rest({ key: 'Hi8h7g.DZi1fQ:C4aCqN_ORbwsRXVWzq8qEDGpHDRD7vbfwI0reJsX4S4' });
   const lobby = client.channels.get('tictactoe:lobby');
+
+  const fetchData = () => {
+    lobby.presence.get((err, resultPage) => {
+      if (!err && resultPage) {
+        const membersData = resultPage.items;
+        setMembers(membersData);
+        console.log(membersData[0]?.clientId);
+        // Process the presence data as needed
+      } else {
+        console.error('Error fetching presence information:', err);
+      }
+    });
+  };
   
   useEffect(() => {
-    const client = new Ably.Rest({ key: 'Hi8h7g.DZi1fQ:C4aCqN_ORbwsRXVWzq8qEDGpHDRD7vbfwI0reJsX4S4' });
-    const lobby = client.channels.get('tictactoe:lobby');
 
-    const fetchData = () => {
-      lobby.presence.get((err, resultPage) => {
-        if (!err && resultPage) {
-          const membersData = resultPage.items;
-          setMembers(membersData);
-          console.log(membersData[0]?.clientId);
-          // Process the presence data as needed
-        } else {
-          console.error('Error fetching presence information:', err);
-        }
-      });
-    };
 
     fetchData();
 
@@ -46,17 +44,20 @@ function Lobby(){
       // Unsubscribe or clean up as needed
     };
   }, []); // The empty dependency array ensures this effect runs only on mount
-  
-    
+  const handleReload = () => {
+    fetchData(); // Manually fetch data when the button is clicked
+  };
+
 
     // In the lobby, we use presence only for players who have hosted a game and
     // are waiting for an opponent to join. When an opponent joins a game, the
     // host will leave the lobby
     return (     
-      <>
+      <><button onClick={handleReload}>Reload Page</button>
         <ul>
           {members.map((presence:Ably.Types.PresenceMessage) => (
             <li key={presence.clientId}>{presence.clientId}</li>
+
           ))}
         </ul>
       </>
